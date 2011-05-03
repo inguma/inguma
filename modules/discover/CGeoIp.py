2a1b55115b56449732b14447b1784a8ad2d2cee1
Hugo Teso <hugo.teso@gmail.com>
2011-05-03 10:48:49 +0200

    Some fixing at CGeoIP.py CLI interface

--- modules/discover/CGeoIp.py
+++ modules/discover/CGeoIp.py
@@ -33,65 +33,75 @@ class CGeoIp(CIngumaModule):
     target = ""
 
     def help(self):
-        print "target = <target IP>"
-        print "         <if set to \"all\", all IP stored on KB will be used>"
-        print "         <if set to \"download\", GeoIP database will be downloaded>"
+        self.gom.echo("target = <target IP>")
+        self.gom.echo("         <if set to \"all\", all IP stored on KB will be used>")
+        self.gom.echo("         <if set to \"download\", GeoIP database will be downloaded>")
 
     def run(self):
         try:
             import GeoIP
         except:
-            print "No GeoIp library found, please install it"
+            self.gom.echo("No GeoIp library found, please install it")
             sys.exit(0)
 
         if self.target == "download":
-
-            self.GEOIP_DIR='data/'        
-            self.INGUMA_DIR = os.getcwd()
-    
-            page = "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz"
-            self.gom.echo( "Downloading " + page, False )
-            urllib.urlretrieve(page, "data/GeoLiteCity.dat.gz")
-    
-            # Extract DB and remove original file
-            self.gom.echo( "Extracting files...", False )
-            gz = gzip.open("data/GeoLiteCity.dat.gz")
-            db = gz.read()
-            gz.close()
-            os.remove("data/GeoLiteCity.dat.gz")
-            geodb = open('data/GeoLiteCity.dat', 'w')
-            geodb.write(db)
-            geodb.close()
-            self.gom.echo( "Operation Complete", False )
-
-            return True
-
+            self.downloadDB()
         elif self.target == "all":
             targets = self.user_data['hosts']
         else:
             targets = [self.target]
 
-        gi  = GeoIP.open('data/GeoLiteCity.dat', GeoIP.GEOIP_STANDARD)
-        print '%-15s  |  %15s %15s %15s %15s %15s ' % ('IP', 'Latitude', 'Longitude', 'Country', 'City', 'Region')
-        print '+----------------+--------------------------------------------------------------------------------+'
-        for ip in targets:
-            try:
-                gir = gi.record_by_addr(ip)
-                ### look up the ip
-                lat = gir['latitude']
-                lon = gir['longitude']
-                country = gir['country_name']
-                region = gir['region']
-                city = gir['city']
-#                print ip, lat, lon, country, region, city
-                print '%-15s ==> %15s %15s %15s %15s %15s ' % (ip, lat, lon, country, city, region)
-            except:
-                print "%-15s ==>" % (ip)
-                #pass
-        print '+-------------------------------------------------------------------------------------------------+'
-        
-        return False
+        if self.checkDB() == False:
+            self.gom.echo('GeoIP database not found, install it setting target = \"download\" and running geoip again')
+        else:
+            gi  = GeoIP.open('data/GeoLiteCity.dat', GeoIP.GEOIP_STANDARD)
+            self.gom.echo('%-15s  |  %15s %15s %15s %15s %15s ' % ('IP', 'Latitude', 'Longitude', 'Country', 'City', 'Region'))
+            self.gom.echo('+----------------+--------------------------------------------------------------------------------+')
+            for ip in targets:
+                try:
+                    gir = gi.record_by_addr(ip)
+                    ### look up the ip
+                    lat = gir['latitude']
+                    lon = gir['longitude']
+                    country = gir['country_name']
+                    region = gir['region']
+                    city = gir['city']
+#                    self.gom.echo(ip, lat, lon, country, region, city)
+                    self.gom.echo('%-15s ==> %15s %15s %15s %15s %15s ' % (ip, lat, lon, country, city, region))
+                except:
+                    self.gom.echo("%-15s ==>" % (ip))
+                    #pass
+            self.gom.echo('+-------------------------------------------------------------------------------------------------+')
+            
+            return False
+
+    def downloadDB(self):
+        self.GEOIP_DIR='data/'        
+        self.INGUMA_DIR = os.getcwd()
+    
+        page = "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz"
+        self.gom.echo("Downloading " + page, False )
+        urllib.urlretrieve(page, "data/GeoLiteCity.dat.gz")
+    
+        # Extract DB and remove original file
+        self.gom.echo("Extracting files...", False)
+        gz = gzip.open("data/GeoLiteCity.dat.gz")
+        db = gz.read()
+        gz.close()
+        os.remove("data/GeoLiteCity.dat.gz")
+        geodb = open('data/GeoLiteCity.dat', 'w')
+        geodb.write(db)
+        geodb.close()
+        self.gom.echo("Operation Complete", False)
+
+        return True
+
+    def checkDB(self):
+        if os.path.isfile('data/GeoLiteCity.dat'):
+            return True
+        else:
+            return False
 
     def printSummary(self):
-        """ If the method run of the module returns True printSummary will called after """
+        """ If the method run of the module returns True, printSummary will called after """
         pass
