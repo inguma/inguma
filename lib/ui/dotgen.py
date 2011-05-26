4e875ec5cbcf6814593b7205ad2884bced7265fe
Hugo Teso <hugo.teso@gmail.com>
2011-05-26 18:30:33 +0200

    Fixes #3 Modified dotgen to avoid overlaping. Removing "fixedsize" makes the node grow to fit the label.

--- lib/ui/dotgen.py
+++ lib/ui/dotgen.py
@@ -17,7 +17,6 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
-import sys
 import lib.ui.config as config
 
 def generate_dot(kb, localip, gw, direction='TD'):
@@ -147,13 +146,13 @@ def generate_folded(kb):
     dotcode = '''
     graph G {
         graph [ overlap="scale", bgcolor="#475672", concentrate="true", root="Internet"]
-            node [color=azure3, fontcolor=white, fillcolor="#373D49", shape=circle, style=filled, fixedsize=true, height=0.9,width=0.9];
+            node [color=azure3, fontcolor=white, fillcolor="#373D49", shape=circle, style=filled, height=0.9,width=0.9];
 
-            "Internet" [style=filled, fillcolor="#5E82C6", fixedsize=true, height=1.0,width=1.0, shape=doublecircle]
+            "Internet" [style=filled, fillcolor="#5E82C6", height=1.0,width=1.0, shape=doublecircle]
     '''
 
     for target in kb['targets']:
-        dotcode += '"' + target + '" [style=filled, fillcolor="#373D49", fixedsize=true, height=0.9,width=0.9, URL="' + target + '"]\n'
+        dotcode += '"' + target + '" [style=filled, fillcolor="#373D49", height=0.9,width=0.9, URL="' + target + '"]\n'
         dotcode += '"Internet" -- "' + target + '" [len=1.50, color=azure3];\n'
 
     dotcode += '\n}'
@@ -211,13 +210,13 @@ def graph_to_from(kb, type):
     dotcode = '''
     graph G {
         graph [ overlap="scale", bgcolor="#475672", concentrate="true", root="Invisnode"]
-		    node [color=azure3, fontcolor=white, fillcolor="#373D49", shape=circle, style=filled, fixedsize=1, height=0.7,width=0.7];
+		    node [color=azure3, fontcolor=white, fillcolor="#373D49", shape=circle, style=filled, height=0.7,width=0.7];
     '''
     #bgcolor="#475672"
-    dotcode += '"Invisnode" [style=invis, fixedsize=1, height=1, width=1, shape=circle]\n'
+    dotcode += '"Invisnode" [style=invis, height=1, width=1, shape=circle]\n'
     if type == 'ports_ip':
         for target in kb['targets']:
-            dotcode += '"' + target + '" [shape="doublecircle", style=filled, fillcolor="#5E82C6", fixedsize=1, height=0.9, width=0.9, URL="' + target + '"]\n'
+            dotcode += '"' + target + '" [shape="doublecircle", style=filled, fillcolor="#5E82C6", height=0.9, width=0.9, URL="' + target + '"]\n'
             dotcode += '"Invisnode" -- "' + target + '" [style=invis]\n'
             if target + '_tcp_ports' in kb:
                 for port in kb[target + '_tcp_ports']:
@@ -236,7 +235,7 @@ def graph_to_from(kb, type):
         for target in kb['targets']:
             try:
                 for port in kb[target + '_tcp_ports']:
-                    dotcode += '"' + str(port) + '" [shape="doublecircle", style=filled, fillcolor="#5E82C6", fixedsize=1, height=0.7,width=0.7]\n'
+                    dotcode += '"' + str(port) + '" [shape="doublecircle", style=filled, fillcolor="#5E82C6", height=0.7,width=0.7]\n'
                     dotcode += '"' + str(port) + '_' + target + '" [label="' + target + '"]\n'
                     dotcode += '"' + str(port) + '" -- "' + str(port) + '_' + target + '" [len=1.25, color=azure3];\n'
             except:
@@ -246,7 +245,7 @@ def graph_to_from(kb, type):
     elif type == 'ports_vuln':
         # Add a node for each target and an invisnode to join them
         for target in kb['targets']:
-            dotcode += '"' + target + '" [shape="doublecircle", style=filled, fillcolor="#5E82C6", fixedsize=1, height=0.9, width=0.9, URL="' + target + '"]\n'
+            dotcode += '"' + target + '" [shape="doublecircle", style=filled, fillcolor="#5E82C6", height=0.9, width=0.9, URL="' + target + '"]\n'
             dotcode += '"Invisnode" -- "' + target + '" [style=invis]\n'
 
             if target + '_tcp_ports' in kb:
@@ -265,7 +264,7 @@ def graph_to_from(kb, type):
                         # For each vuln on that port...
                         for vuln in kb[target + "_" + str(port) + '-web-vulns']:
                             # Add a vuln node and join with the target port
-                            dotcode += '"' + vuln[0] + str(vuln_id) + '" [style=filled, fillcolor=indianred4, fixedsize=1, height=0.9, width=0.9, label=\"OSVDB:' + vuln[0] + '\", URL="OSVDB:' + vuln[0] + '"]\n'
+                            dotcode += '"' + vuln[0] + str(vuln_id) + '" [style=filled, fillcolor=indianred4, height=0.9, width=0.9, label=\"OSVDB:' + vuln[0] + '\", URL="OSVDB:' + vuln[0] + '"]\n'
                             dotcode += '"' + target + '_' + str(port) + '" -- "' + vuln[0] + str(vuln_id) + '" [len=1.25, color=azure3];\n'
                             vuln_id += 1
 
@@ -273,7 +272,7 @@ def graph_to_from(kb, type):
                         vuln_id = 0
                         for vuln in kb[target + "_" + str(port) + '-web-vulns']:
                             # Add a vuln poc node and join with the vuln id
-                            dotcode += '"' + vuln[0] + str(vuln_id) + '_poc" [shape=record, color=azure3, fontcolor=azure3, style="filled,rounded", fillcolor="#373D49" fixedsize=false label="' + vuln[1] + '", URL="poc_http://' + target + ':' + port + vuln[1] + '"]\n'
+                            dotcode += '"' + vuln[0] + str(vuln_id) + '_poc" [shape=record, color=azure3, fontcolor=azure3, style="filled,rounded", fillcolor="#373D49" label="' + vuln[1] + '", URL="poc_http://' + target + ':' + port + vuln[1] + '"]\n'
                             dotcode += '"' + vuln[0] + str(vuln_id) + '" -- "' + vuln[0] + str(vuln_id) + '_poc"  [len=1.25, color=azure3];\n'
 
                             vuln_id += 1
