f9dea94f48fa090591a5bb7c614f793a61aeaae0
David Martínez Moreno <ender@debian.org>
2011-08-08 12:20:06 -0700

    Converted Bluetooth to CIngumaDiscoverModule, unified syntax, fixed obvious bugs and converted it to gom.echo() (#61).

--- modules/discover/CBluetooth.py
+++ modules/discover/CBluetooth.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 ##      CBluetooth.py
 #       
 #       Copyright 2010 Hugo Teso <hugo.teso@gmail.com>
@@ -19,22 +17,21 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
-import sys
 try:
     import bluetooth
 except:
-    print "module bluetooth (pybluez) not found"
+    print "Module bluetooth (pybluez) not found."
 
-from lib.module import CIngumaModule
+from lib.module import CIngumaDiscoverModule
 
 name = "bluetooth"
 brief_description = "A simple bluetooth scanner"
-type = "discover" # The type of a module, currently, can only be 'gather', 'exploit', 'discover', 'fuzz' or 'brute'
+type = "discover"
 
 globals = ['mode', ]
 
-class CBluetooth(CIngumaModule):
-    """ The example module. The main class will always starts with the character "C". Any other class will be ignored """
+class CBluetooth(CIngumaDiscoverModule):
+    """ Module for Bluetooth operations. """
 
     # Default values
     mode = 'discover'
@@ -42,49 +39,49 @@ class CBluetooth(CIngumaModule):
 
     def help(self):
         """ This is the entry point for info <module> """
-        print "mode = <discover|gather>"
-        print "       <discover: search for bluetooth devices>"
-        print "       <gather: gather services of specified device (target)>"
-        print "target = <all|localhost|address>"
-        print "         <all: gather services of all devices available>"
-        print "         <localhost: scan localhost services>"
-        print "         <address: scan specified addres>"
+        self.gom.echo("mode = <discover|gather>")
+        self.gom.echo("       <discover: search for bluetooth devices>")
+        self.gom.echo("       <gather: gather services of specified device (target)>")
+        self.gom.echo("target = <all|localhost|address>")
+        self.gom.echo("         <all: gather services of all devices available>")
+        self.gom.echo("         <localhost: scan localhost services>")
+        self.gom.echo("         <address: scan specified addres>")
 
     def run(self):
         """ This is the main entry point of the module """
 
         if self.mode=='discover':
-            self.gom.echo( "Searching Bluetooth devices..." )
+            self.gom.echo("Searching Bluetooth devices...")
             nearby_devices = bluetooth.discover_devices(lookup_names = True)
-            self.gom.echo( "found " + str(len(nearby_devices)) + " devices" )
+            self.gom.echo("found " + str(len(nearby_devices)) + " devices")
             for name, addr in nearby_devices:
-                self.gom.echo( str(addr) + " " + str(name) )
+                self.gom.echo(str(addr) + " " + str(name))
                 self.addToDict("blue_hosts", addr)
                 self.addToDict(addr + "_addr", name)
             return False
 
         elif self.mode=='gather':
-            target = None
+            self.target = None
             services = bluetooth.find_service(address=self.target)
             if len(services) > 0:
-                self.gom.echo( "found " + str(len(services)) + " services on " + str(self.target))
-                self.gom.echo( "" )
+                self.gom.echo("found " + str(len(services)) + " services on " + str(self.target))
+                self.gom.echo("")
             else:
-                self.gom.echo( "no services found" )
+                self.gom.echo("no services found")
             
             for svc in services:
-                self.gom.echo( "Service Name: " +  str(svc["name"] ))
-                self.gom.echo( "    Host:        " + str(svc["host"] ))
-                self.gom.echo( "    Description: " + str(svc["description"] ))
-                self.gom.echo( "    Provided By: " + str(svc["provider"] ))
-                self.gom.echo( "    Protocol:    " + str(svc["protocol"] ))
-                self.gom.echo( "    channel/PSM: " + str(svc["port"] ))
-                self.gom.echo( "    str(svc classes:  " + str(svc["service-classes"] ))
-                self.gom.echo( "    profiles:     " + str(svc["profiles"] ))
-                self.gom.echo( "    service id:   " + str(svc["service-id"] ))
-                self.gom.echo( "" )
+                self.gom.echo("Service Name: " +  str(svc["name"]))
+                self.gom.echo("    Host:        " + str(svc["host"]))
+                self.gom.echo("    Description: " + str(svc["description"]))
+                self.gom.echo("    Provided By: " + str(svc["provider"]))
+                self.gom.echo("    Protocol:    " + str(svc["protocol"]))
+                self.gom.echo("    channel/PSM: " + str(svc["port"]))
+                self.gom.echo("    str(svc classes:  " + str(svc["service-classes"]))
+                self.gom.echo("    profiles:     " + str(svc["profiles"]))
+                self.gom.echo("    service id:   " + str(svc["service-id"]))
+                self.gom.echo()
             return False
 
         else:
-            self.gom.echo( "Mode " + srt(self.mode) + " not valid" )
+            self.gom.echo("Mode " + str(self.mode) + " not valid.")
             return False
