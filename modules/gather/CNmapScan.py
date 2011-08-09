3ca79907e7a57b3d824bb3a8fe6c88e0360c5d61
David Martínez Moreno <ender@debian.org>
2011-08-08 22:50:36 -0700

    Converted NmapScan to CIngumaGatherModule, converted functions to PEP8 and sorted them, migrated it to gom.echo() and the rest of niceties (#61).

--- modules/gather/CNmapScan.py
+++ modules/gather/CNmapScan.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 ##      CNmapScan.py
 #       
 #       Copyright 2010 Hugo Teso <hugo.teso@gmail.com>
@@ -19,80 +17,35 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
-import sys, os, string
+import os, string
 import lib.ui.nmapParser as nmapParser
-from lib.module import CIngumaModule
+from lib.module import CIngumaGatherModule
 
 name = "nmapscan"
 brief_description = "A module for port scanning using Nmap"
 type = "gather"
 
-class CNmapScan(CIngumaModule):
+class CNmapScan(CIngumaGatherModule):
     interactive = True
 
     def help(self):
-        print "A module for port scanning using Nmap"
+        self.gom.echo("A module for port scanning using Nmap")
+
+    def help_nmap(self):
+        self.gom.echo(os.popen('nmap --help').read())
 
     def show_help(self):
-        print 
-        print "Inguma's Nmap Interface Help"
-        print "------------------------------"
-        print
-        print "help                     Show this help"
-        print "nmaphelp                 Show Nmap's help"
-        print "nmap <options>           Execute Nmap with options specified"
-        print "exit                     Exit from nmapscan interface"
-        print 
+        self.gom.echo()
+        self.gom.echo("Inguma's Nmap Interface Help")
+        self.gom.echo("------------------------------")
+        self.gom.echo()
+        self.gom.echo("help                     Show this help")
+        self.gom.echo("nmaphelp                 Show Nmap's help")
+        self.gom.echo("nmap <options>           Execute Nmap with options specified")
+        self.gom.echo("exit                     Exit from nmapscan interface")
+        self.gom.echo()
 
-    def HelpNmap(self):
-        self.gom.echo( os.popen('nmap --help').read() )
-
-    def runNmap(self,  options):
-        os.popen(options + " -oX /tmp/nmapxml.xml")
-        nmapxml = open('/tmp/nmapxml.xml')
-        
-        outputs = nmapParser.parseNmap('/tmp/nmapxml.xml')
-        nmapxml.close()
-        os.remove('/tmp/nmapxml.xml')
-        
-        for output in outputs:
-            
-            # Add a new target, hostname and OS
-            self.addToDict( 'targets', output['hostip'] )
-            self.gom.echo( "Host IP:\t" + output['hostip'] )
-            if 'hostname' in output.keys():
-                self.addToDict( output['hostip'] + '_name', output['hostname'] )
-                self.gom.echo( "Host Name:\t" + output['hostname'] )
-            if 'os' in output.keys():
-                self.addToDict( output['hostip'] + '_os', output['os'] )
-                self.gom.echo( "Host OS:\t" + output['os'] )
-        
-            # Add Open ports and services
-            self.gom.echo( "Host Ports:" )
-            for port in output['ports'].keys():
-                if output['ports'][port][0] == 'open':
-                    self.gom.echo( "\n\tPort: " + port )
-                    self.addToDict(output['hostip'] + "_tcp_ports", port)
-                    try:
-                        self.addToDict(output['hostip'] + "_" + port + '-info', output['ports'][port][1])
-                        self.addToDict(output['hostip'] + "_" + port + '-info', output['ports'][port][2])
-                        self.gom.echo( "\tInfo 1: " + output['ports'][port][1] )
-                        self.gom.echo( "\tInfo 2: " + output['ports'][port][2] )
-                    except:
-                        pass
-        
-            # Add traceroute
-            self.gom.echo( "\nTraceroute:" )
-            for host in output['hops']:
-                self.addToDict( 'hosts', host[0] )
-                if host[1] != '':
-                    self.addToDict( host[0] + '_name', host[1] )
-                    self.gom.echo( "\t" + host[0] + "\t-> " + host[1] )
-                else:
-                    self.gom.echo( "\t" + host[0] )
-                self.addToDict( output['hostip'] + '_trace', host[0] )
-
-    def runLoop(self):
+    def run(self):
         import lib.ui.cli.core as CLIcore
 
         while 1:
@@ -106,14 +59,55 @@ class CNmapScan(CIngumaModule):
                 continue
             elif words[0].lower() == "nmap" and len(words)>1:
                 mystring = string.join(words, ' ')
-                self.runNmap(mystring)
+                self.run_nmap(mystring)
             elif words[0].lower() == "nmaphelp":
-                self.HelpNmap()
+                self.help_nmap()
             else:
-                print "Unknown option or command '%s'" % res
-
-    def run(self):
-
-        self.runLoop()
+                self.gom.echo("Unknown option or command '%s'" % res)
 
         return True
+
+    def run_nmap(self, options):
+        os.popen(options + " -oX /tmp/nmapxml.xml")
+        nmapxml = open('/tmp/nmapxml.xml')
+        
+        outputs = nmapParser.parseNmap('/tmp/nmapxml.xml')
+        nmapxml.close()
+        os.remove('/tmp/nmapxml.xml')
+        
+        for output in outputs:
+            
+            # Add a new target, hostname and OS
+            self.add_data_to_kb( 'targets', output['hostip'] )
+            self.gom.echo("Host IP:\t" + output['hostip'])
+            if 'hostname' in output.keys():
+                self.add_data_to_kb( output['hostip'] + '_name', output['hostname'] )
+                self.gom.echo("Host Name:\t" + output['hostname'])
+            if 'os' in output.keys():
+                self.add_data_to_kb( output['hostip'] + '_os', output['os'] )
+                self.gom.echo("Host OS:\t" + output['os'])
+        
+            # Add open ports and services.
+            self.gom.echo("Host Ports:")
+            for port in output['ports'].keys():
+                if output['ports'][port][0] == 'open':
+                    self.gom.echo("\n\tPort: " + port)
+                    self.add_data_to_kb(output['hostip'] + "_tcp_ports", port)
+                    try:
+                        self.add_data_to_kb(output['hostip'] + "_" + port + '-info', output['ports'][port][1])
+                        self.add_data_to_kb(output['hostip'] + "_" + port + '-info', output['ports'][port][2])
+                        self.gom.echo("\tInfo 1: " + output['ports'][port][1])
+                        self.gom.echo("\tInfo 2: " + output['ports'][port][2])
+                    except:
+                        pass
+        
+            # Add traceroute
+            self.gom.echo("\nTraceroute:")
+            for host in output['hops']:
+                self.add_data_to_kb( 'hosts', host[0] )
+                if host[1] != '':
+                    self.add_data_to_kb(host[0] + '_name', host[1])
+                    self.gom.echo("\t" + host[0] + "\t-> " + host[1])
+                else:
+                    self.gom.echo("\t" + host[0])
+                self.add_data_to_kb(output['hostip'] + '_trace', host[0])
