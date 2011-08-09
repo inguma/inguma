54ad6609eb72cc7dd03ecb3431598a1798e9d38c
David Martínez Moreno <ender@debian.org>
2011-08-08 23:19:06 -0700

    Converted Db2DiscoverModule to CIngumaDiscoverModule, fixed syntax and splitted reporting section from run() into print_summary() (#61).

--- modules/discover/CDb2DiscoverModule.py
+++ modules/discover/CDb2DiscoverModule.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 ##      Cdb2DiscoverModule.py
 #       
 #       Copyright 2010 Joxean Koret <joxeankoret@yahoo.es>
@@ -19,49 +17,50 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
-import sys
-
-from lib.module import CIngumaModule
+from lib.module import CIngumaDiscoverModule
 from lib.libdb2 import CDb2Discover
 
 name = "db2discover"
 brief_description = "IBM DB2 database servers discover module"
 type = "discover"
 
-class CDb2DiscoverModule(CIngumaModule):
+class CDb2DiscoverModule(CIngumaDiscoverModule):
 
     def help(self):
-        print "target = <target host or network>"
-        print "port = <target port>"
-        print "timeout = <timeout>"
+        self.gom.echo("target = <target host or network>")
+        self.gom.echo("port = <target port>")
+        self.gom.echo("timeout = <timeout>")
+
+    def print_summary(self):
+        for server in self.answer:
+            self.gom.echo("[+] IBM DB2 Server at " + str(server[0][0]) + ": " + str(server[0][1]))
+            self.gom.echo("  Version   : " + str(server[1]))
+            self.gom.echo("  Hostname  : " + str(server[2]))
+            self.gom.echo("  Servername: " + str(server[3]))
+            self.gom.echo()
+            self.add_data_to_kb("hosts",  server[0][0])
+            self.add_data_to_kb(server[0][0] + "_db2server",  (server[1],  server[2],  server[3]))
+
+        self.gom.echo("Total of " + str(len(self.answer)) + " IBM DB2 Server(s) found.")
+        
+        return len(ret) > 0
 
     def run(self):
         
         if self.target == "":
             self.target = "<broadcast>"
-            self.gom.echo ("[i] Using broadcast address 255.255.255.255" )
+            self.gom.echo("[i] Using broadcast address 255.255.255.255")
         
         if self.port == 0 or self.port == None:
             self.port = 553
         
         if self.port != 553:
-            self.gom.echo( "[!] Warning! using non standard port " + self.port )
+            self.gom.echo("[!] Warning! Using non-standard port " + self.port)
         
         db2Discover = CDb2Discover()
         db2Discover.target = self.target
         db2Discover.port = self.port
         db2Discover.verbose = False
-        ret = db2Discover.discover()
+        self.answer = db2Discover.discover()
 
-        for server in ret:
-            self.gom.echo( "[+] IBM DB2 Server at " + str(server[0][0]) + ": " + str(server[0][1]) )
-            self.gom.echo( "  Version   : " + str(server[1]) )
-            self.gom.echo( "  Hostname  : " + str(server[2]) )
-            self.gom.echo( "  Servername: " + str(server[3]) )
-            self.gom.echo( "" )
-            self.addToDict("hosts",  server[0][0])
-            self.addToDict(server[0][0] + "_db2server",  (server[1],  server[2],  server[3]))
-
-        self.gom.echo( "Total of " + str(len(ret)) + " IBM DB2 Server(s) found." )
-        
-        return len(ret) > 0
+        return True
