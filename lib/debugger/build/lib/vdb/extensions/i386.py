ebff9ccca257c7bcc876b7d3733af8aacf7c3e9c
Hugo Teso <hugo.teso@gmail.com>
2011-09-14 10:48:15 +0200

    Removed debugger from the release

--- lib/debugger/build/lib/vdb/extensions/i386.py
+++
@@ -1,7 +0,0 @@
-
-def vdbExtension(vdb, trace):
-    vdb.setConfig("Aliases","db","mem -F Bytes")
-    vdb.setConfig("Aliases","dw","mem -F u_int_16")
-    vdb.setConfig("Aliases","dd","mem -F u_int_32")
-    vdb.setConfig("Aliases","dq","mem -F u_int_64")
-
