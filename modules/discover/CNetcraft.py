6f5945ef34499576a0b921263315b959650c5ba4
David Martínez Moreno <ender@debian.org>
2011-12-23 20:50:12 -0800

    Converted the rest (CNetcraft.py, CPromisc.py, CSubdomainer.py, CTcpPing.py, CTraceroute.py, CUdpPing.py, CWhois.py) to CIngumaDiscoverModule, PEP8, whitespace cleaning, blahblahblah... (#61).

--- modules/discover/CNetcraft.py
+++ modules/discover/CNetcraft.py
@@ -1,17 +1,17 @@
 ##      CNetcraft.py
-#       
+#
 #       Copyright 2010 Hugo Teso <hugo.teso@gmail.com>
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -41,7 +41,7 @@ class SimpleHTMLParser(HTMLParser):
             self.ignoreIt = False
 
     def handle_data(self, data):
-    
+
         if len(data) > 1 and data.lower().find("uptime graph") == -1:
 
             if data[0:1] == '\r' or data[0:1] == '\n':
@@ -52,7 +52,7 @@ class SimpleHTMLParser(HTMLParser):
 class CNetcraft(CIngumaDiscoverModule):
 
     def NetcraftHtmlParser(self, data):
-        
+
         html_parser = SimpleHTMLParser()
         html_parser.feed(data)
         html_parser.close()
@@ -73,7 +73,7 @@ class CNetcraft(CIngumaDiscoverModule):
 
     def help(self):
         """ Method called when 'help <module> is executed from the command line. """
-        self.gom.echo("target = \"<target host or network>\"")
+        self.gom.echo('target = "<target host or network>"')
 
     def run(self):
         try:
@@ -97,9 +97,8 @@ class CNetcraft(CIngumaDiscoverModule):
         end = len(self.data)
         for x in range(start, end):
             try:
-                self.gom.echo( str(self.data[start]) + ':\t\t' + str(self.data[start + 1]) )
+                self.gom.echo(str(self.data[start]) + ':\t\t' + str(self.data[start + 1]))
                 self.addToDict(self.target + "_netcraft", [self.data[start], self.data[start + 1]])
                 start = start + 2
             except:
                 return False
-
--- modules/discover/CUdpPing.py
+++ modules/discover/CUdpPing.py
@@ -1,19 +1,17 @@
-#!/usr/bin/python
-
 ##      CUdpPing.py
-#       
+#
 #       Copyright 2010 Hugo Teso <hugo.teso@gmail.com>
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -47,70 +45,55 @@ class CUdpPing(CIngumaModule):
     dict = None
 
     def help(self):
-        print "target = <target host or network>"
-        print "timeout = <timeout>"
-        print "waitTime = <wait time between packets>"
-        print "port = <destination port to ping>"
-        print "iface = <iface>"
+        self.gom.echo('target = <target host or network>')
+        self.gom.echo('timeout = <timeout>')
+        self.gom.echo('waitTime = <wait time between packets>')
+        self.gom.echo('port = <destination port to ping>')
+        self.gom.echo('iface = <iface>')
 
     def run(self):
         if not bHasScapy:
-            print "No scapy support :("
+            self.gom.echo('No scapy support :(')
             return False
         self.results = {}
         self.up = {}
         self.down = {}
 
         target = scapy.IP(dst=self.target)
-        
-        self.gom.echo( "Sending probe to\t" + str(target.dst) + "\tusing port\t" + str(self.port) )
+
+        self.gom.echo("Sending probe to\t" + str(target.dst) + "\tusing port\t" + str(self.port))
         p = scapy.IP(dst=target.dst)/scapy.UDP(dport=self.port)
 
         ans, unans = scapy.sr(p, timeout=self.timeout, iface=self.iface, retry=0)
 
-#            self.gom.echo( ans.summary( lambda(s,r) : r.sprintf("%IP.src% is alive") ) )
+#            self.gom.echo(ans.summary( lambda(s,r) : r.sprintf("%IP.src% is alive") ))
 
         if ans:
             for a in ans:
                 self.up[len(self.up)+1] = a[0][0].dst
-                self.addToDict("alive", a[0][0].dst)
-                self.addToDict("hosts", a[0][0].dst)
-                self.addToDict("targets", a[0][0].dst)
-                #self.addToDict(ans[0][0].dst + "_trace", ans[0][0].dst)
+                self.add_data_to_kb("alive", a[0][0].dst)
+                self.add_data_to_kb("hosts", a[0][0].dst)
+                self.add_data_to_kb("targets", a[0][0].dst)
+                #self.add_data_to_kb(ans[0][0].dst + "_trace", ans[0][0].dst)
 #                else:
 #                    self.down[len(self.up)+1] = ans[0][0].dst
-#                    self.gom.echo( "Answer of type " + str(icmptypes[ans[0][0].type]) + " from " + str(ans[0][0].dst) )
+#                    self.gom.echo("Answer of type " + str(icmptypes[ans[0][0].type]) + " from " + str(ans[0][0].dst))
 
         self.results = self.up
         return True
-    
-    def printSummary(self):
+
+    def print_summary(self):
         if len(self.results) == 0:
             return
 
         i = 0
-        self.gom.echo( "" )
-        self.gom.echo( "Discovered hosts" )
-        self.gom.echo( "----------------" )
-        self.gom.echo( "" )
+        self.gom.echo()
+        self.gom.echo("Discovered hosts")
+        self.gom.echo("----------------")
+        self.gom.echo()
 
         for res in self.results:
             i += 1
-            self.gom.echo( "Found host " + str(i) + "\t" + str(self.results[res]) )
+            self.gom.echo("Found host " + str(i) + "\t" + str(self.results[res]))
 
-        print
-
-if __name__ == "__main__":
-    import sys
-    objHostUp = CHostUp()
-    objHostUp.iface = "eth0"
-
-    if len(sys.argv) == 1:
-        objHostUp.target = "192.168.1.1"
-    else:
-        objHostUp.target = sys.argv[1]
-
-    objHostUp.run()
-
-    for host in objHostUp.up:
-        self.gom.echo( "Host " + str(host) +" is up" )
+        self.gom.echo()
