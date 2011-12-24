97fe8c42f1390351a9d74841e753cfbd653b29f5
David Martínez Moreno <ender@debian.org>
2011-12-23 19:40:02 -0800

    Whitespace cleanup, and fixed alignment of description.

--- modules/discover/CAsnQuery.py
+++ modules/discover/CAsnQuery.py
@@ -1,18 +1,18 @@
 ##      CAsnQuery.py
-#       
+#
 #       Copyright 2010 Hugo Teso <hugo.teso@gmail.com>
 #       Copyright 2010 Joxean Koret <joxeankoret@yahoo.es>
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -22,7 +22,7 @@ import scapy.all as scapy
 from lib.module import CIngumaDiscoverModule
 
 name = "asn"
-brief_description = "ASN whois database query"
+brief_description = "\tASN whois database query"
 type = "discover"
 
 class CAsnQuery(CIngumaDiscoverModule):
